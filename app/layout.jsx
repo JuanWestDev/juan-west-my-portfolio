83248f2558f883f7a83cf4ce648a4b18c64ec1f6
JuanWestDev <juanwestworks@gmail.com>
2025-04-11 10:39:59 +0200

    Test mobile without top section

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,7 +24,6 @@ export default function RootLayout({ children }) {
             <NavigationBar home="/" about="/about" projects="/projects" />
           </header>
 
-          <GradientTopSection />
           {children}
           <section className="mt-15 mb-5 border-[1.5px] rounded-full bg-neutral-800 border-neutral-800 opacity-60 mx-auto animate-grow-size ease-out"></section>
           <footer className=" mx-auto pb-10 container w-full md:w-auto text-neutral-300 px-4 md:px-0 z-50 animate-fade-up">
