7f054707fa80552e115a89b6b0a1b1c05478d689
JuanWestDev <juanwestworks@gmail.com>
2025-04-11 13:01:01 +0200

    Optimize animation performance

--- components/GradientTopSection.jsx
+++ components/GradientTopSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GradientTopSection = () => {
   return (
-    <div className="absolute left-[50%] top-0 -translate-y-[2%] z-0 flex h-[35rem] w-full max-w-[1200px] translate-x-[-50%] items-center justify-center md-[-2%] md:animate-slower">
+    <div className="absolute left-[50%] top-0 -translate-y-[2%] z-0 flex h-[35rem] w-full max-w-[1200px] translate-x-[-50%] items-center justify-center md-[-2%] scale-[1.5] animate-pulse-slow">
       <div className="relative ">
         <div className="gradient-1 absolute left-0 top-0"></div>
         <div className="gradient-2 absolute left-0 top-0 translate-x-[-50%]"></div>
