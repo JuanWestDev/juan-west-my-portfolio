4c836de9472bc9e5477906dbb253a8a3833f028c
juanwestdev <juanwestworks@gmail.com>
2025-05-06 15:24:36 +0200

    about change

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -25,8 +25,8 @@ export default function About() {
       <TopSection
         title="About"
         desc="  I'm a fullstack developer who fell in love with web development and enjoy
-            making a clean user experience. I love a challenge and am always ready to
-            learn about any new techonologies to stay up to date with the current market."
+            making a clean user experience. I love a challenge and always ready to
+            learn about new techonologies to stay up to date with the current market."
       />
       <Technologies />
     </>
