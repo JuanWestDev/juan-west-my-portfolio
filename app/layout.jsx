982f97f84eb3cc9dadcf260f8df35838f3638f0f
Juan West <161832751+JuanWestDev@users.noreply.github.com>
2026-04-17 21:32:53 +0200

    Change script loading strategy to lazyOnload

--- app/layout.jsx
+++ app/layout.jsx
@@ -50,7 +50,7 @@ export default function RootLayout({ children }) {
           <Script
   src="https://factory.lekkerai.co.za/widget/embed.js"
   data-lekkerai="pk_baffefd8aacade20b29395e007af58af"
-  strategy="afterInteractive"
+  strategy="lazyOnload"
 />
       </body>
     </html>
